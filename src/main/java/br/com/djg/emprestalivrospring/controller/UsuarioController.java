a7a133f2f359c8052c416cd1863eaad78a7b74e4
Luiza Neves <luiza.nbs@gmail.com>
2021-05-01 16:37:05 -0300

    removido import que não era usado

--- src/main/java/br/com/djg/emprestalivrospring/controller/UsuarioController.java
+++ src/main/java/br/com/djg/emprestalivrospring/controller/UsuarioController.java
@@ -3,7 +3,6 @@ package br.com.djg.emprestalivrospring.controller;
 import br.com.djg.emprestalivrospring.dominio.Usuario;
 import br.com.djg.emprestalivrospring.servico.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
