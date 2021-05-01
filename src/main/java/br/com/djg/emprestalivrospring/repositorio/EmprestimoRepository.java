764e62c51f8e4c217700369e763fbf20a91f3ed0
Luiza Neves <luiza.nbs@gmail.com>
2021-05-01 15:37:23 -0300

    commit do projeto

---
+++ src/main/java/br/com/djg/emprestalivrospring/repositorio/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+package br.com.djg.emprestalivrospring.repositorio;
+
+import br.com.djg.emprestalivrospring.dominio.Emprestimo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo,Long> {
+
+
+}
